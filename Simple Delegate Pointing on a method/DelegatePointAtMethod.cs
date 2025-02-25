5324b4b97bbbbe78cd81a020336cea60552ca4c4
ReneBossen <renebossen99@gmail.com>
2025-02-25 12:40:13 +0100

    Added delegates

---
+++ Simple Delegate Pointing on a method/DelegatePointAtMethod.cs
@@ -0,0 +1,18 @@
+﻿public delegate void MyDelegate(string message);
+
+public class Example
+{
+    public void ShowMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+public class DelegatePointAtMethod
+{
+    private static void Main(string[] args)
+    {
+        Example example = new Example();
+        MyDelegate del = example.ShowMessage;
+        del("Hello, World!"); // Output: Hello, World!
+    }
+}
---
+++ Simple Delegate with Powerplant/Powerplant.cs
@@ -0,0 +1,34 @@
+﻿public delegate void Warning(int temperature);
+internal class KørPowerPlant {
+    static void Main(string[] args) {
+        PowerPlant plant = new PowerPlant();
+        plant.SetWarning(warningToConsole);
+
+        for (int i = 0; i < 10; i++) {
+            plant.HeatUp();
+        }
+
+        Console.ReadLine();
+    }
+    private static void warningToConsole(int t) {
+        Console.WriteLine("Advarsel, temperaturen er " + t);
+    }
+}
+public class PowerPlant {
+    private Warning _Warning;
+    Random random = new Random();
+
+    public void addWarning(Warning wa) {
+        _Warning += wa;
+    }
+    public void SetWarning(Warning wa) {
+        _Warning = wa;
+    }
+    public void HeatUp() {
+
+        int t = random.Next(100);
+        if (t > 50) {
+            _Warning.Invoke(t);
+        }
+    }
+}
