b85d8b684bac5707d7ec93d96810dc1d706be1a9
ReneBossen <renebossen99@gmail.com>
2025-02-25 12:41:11 +0100

    Added observer pattern

---
+++ Simple use of Observer Pattern/Program.cs
@@ -0,0 +1,27 @@
+﻿//SE DELEGATES --> POWERPLANT FOR BEDRE EKSEMPEL!
+
+public class Subject {
+    public delegate void Notify(); // Delegate type
+    public event Notify OnNotify; // Event using the delegate
+
+    public void DoSomething() {
+        // Her skal der være logik
+        OnNotify?.Invoke(); // Notify observers
+    }
+}
+
+public class Observer {
+    public void React() {
+        Console.WriteLine("Observer notified.");
+    }
+}
+
+public class Run {
+    public static void Main(string[] args) {
+        Subject subject = new Subject();
+        Observer observer = new Observer();
+
+        subject.OnNotify += observer.React; // Subscribe
+        subject.DoSomething(); // Output: Observer notified.
+    }
+}
